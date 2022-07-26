3c4997690be2d74fb30bd329ccd2004b41af3131
Daniel Castillo <dlcastillo3015@gmail.com>
2022-07-26 08:19:25 -0400

    v1.0.1 - Functions to validate any format, an exact value, if a radio button is selected, the maximum, minimum and exact amount of selected options in a checkboxes group, IPv4 format and subnet mask format

--- formly.js
+++ formly.js
@@ -1,10 +1,11 @@
-// Verify the email format
-export const validEmailFormat = (emailInput) => {
-  const EMAIL_REGEX = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
-  let userEmail = emailInput.value;
+/* Formly functions */
 
-  return EMAIL_REGEX.test(userEmail);
-};
+// Verify any format
+const validAnyFormat = (anyInput, REGEX) => REGEX.test(anyInput.value);
+
+// Verify the email format
+export const validEmailFormat = (emailInput) =>
+  validAnyFormat(emailInput, /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/);
 
 // Verify the password format
 export const validPasswordFormat = (
@@ -32,9 +33,8 @@ export const validPasswordFormat = (
       characters +
       ",})"
   );
-  let userPassword = passwordInput.value;
 
-  return PASSWORD_REGEX.test(userPassword);
+  return validAnyFormat(passwordInput, PASSWORD_REGEX);
 };
 
 // Verify the maximum age that the form accepts
@@ -68,50 +68,93 @@ export const validMinAge = (anyInput, min) => {
 };
 
 // Verify if an input is empty
-export const isInputEmpty = (anyInput) => {
-  if (anyInput.value === "") {
-    return true;
-  } else {
-    return false;
-  }
-};
+export const isInputEmpty = (anyInput) =>
+  anyInput.value === "" ? true : false;
 
 // Verify the website format
-export const validWebsiteFormat = (websiteInput) => {
-  const WEBSITE_REGEX =
-    /https?:\/\/(www\.)?[-a-zA-Z0-9@:%._\+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_\+.~#?&//=]*)/;
-  let userWebsite = websiteInput.value;
-
-  return WEBSITE_REGEX.test(userWebsite);
-};
+export const validWebsiteFormat = (websiteInput) =>
+  validAnyFormat(
+    websiteInput,
+    /https?:\/\/(www\.)?[-a-zA-Z0-9@:%._\+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_\+.~#?&//=]*)/
+  );
 
 // Verify the maximum amount of characters that the form accepts
-export const validMaxChar = (anyInput, max) => {
-  if (anyInput.value.length > max) {
-    return false;
-  } else {
-    return true;
-  }
-};
+export const validMaxChar = (anyInput, max) =>
+  anyInput.value.length > max ? false : true;
 
 // Verify the minimum amount of characters that the form accepts
-export const validMinChar = (anyInput, min) => {
-  if (anyInput.value.length < min) {
-    return false;
-  } else {
-    return true;
-  }
-};
+export const validMinChar = (anyInput, min) =>
+  anyInput.value.length < min ? false : true;
 
 // Verify the username format of Twitter or GitHub
 export const validUsername = (usernameInput, sm) => {
-  let username = usernameInput.value;
-
   if (sm === "TW") {
-    const TW_REGEX = /^[A-Za-z0-9_]{1,15}$/;
-    return TW_REGEX.test(username);
+    return validAnyFormat(usernameInput, /^[A-Za-z0-9_]{1,15}$/);
   } else if (sm === "GH") {
-    const GH_REGEX = /^[a-z\d](?:[a-z\d]|-(?=[a-z\d])){0,38}$/i;
-    return GH_REGEX.test(username);
+    return validAnyFormat(
+      usernameInput,
+      /^[a-z\d](?:[a-z\d]|-(?=[a-z\d])){0,38}$/i
+    );
   }
+};
+
+// Verify if an input has an exact value
+export const validExactInput = (anyInput, exactValue) =>
+  anyInput.value === exactValue ? true : false;
+
+// Verify if an option in a group of radio buttons is selected
+export const RadioButtonSelected = (nameAttr) => {
+  let allInputs = document.getElementsByTagName("input");
+
+  for (let i = 0; i < allInputs.length; i++) {
+    if (allInputs[i].name === nameAttr && allInputs[i].checked === true) {
+      return true;
+    }
+  }
+
+  return false;
+};
+
+// Verify the maximum amount of selected options that a group of checkbox accepts
+export const validMaxCheckbox = (nameAttr, max) =>
+  totalChecked(nameAttr) > max ? false : true;
+
+// Verify the minimum amount of selected options that a group of checkbox accepts
+export const validMinCheckbox = (nameAttr, min) =>
+  totalChecked(nameAttr) < min ? false : true;
+
+// Verify the exact amount of selected options that a group of checkbox accepts
+export const validExactCheckbox = (nameAttr, exact) =>
+  totalChecked(nameAttr) === exact ? true : false;
+
+// Verify the IPv4 format
+export const validIpv4Format = (IpInput) =>
+  validAnyFormat(
+    IpInput,
+    /^(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)$/
+  );
+
+// Verify the subnet mask format
+export const validMaskFormat = (MaskInput) =>
+  validAnyFormat(
+    MaskInput,
+    /^(((255\.){3}(255|254|252|248|240|224|192|128|0+))|((255\.){2}(255|254|252|248|240|224|192|128|0+)\.0)|((255\.)(255|254|252|248|240|224|192|128|0+)(\.0+){2})|((255|254|252|248|240|224|192|128|0+)(\.0+){3}))$/
+  );
+
+export default validAnyFormat;
+
+/* Auxiliary function */
+
+// Returns the total of selected checkboxes in a group
+const totalChecked = (nameAttr) => {
+  let allInputs = document.getElementsByTagName("input");
+  let total = 0;
+
+  for (let i = 0; i < allInputs.length; i++) {
+    if (allInputs[i].name === nameAttr && allInputs[i].checked === true) {
+      total += 1;
+    }
+  }
+
+  return total;
 };
