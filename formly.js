c126b63586cb15ab55f7c72af9883483b779a014
Daniel Castillo <dlcastillo3015@gmail.com>
2022-08-01 05:39:20 -0400

    v1.0.2 Functions to verify the minimum and maximum age, the minimum and maximum amount of selected options that a group of checkbox accepts, the minimum and maximum amount of characters that the form accepts

--- formly.js
+++ formly.js
@@ -1,11 +1,48 @@
-/* Formly functions */
+/* Formly general function */
 
 // Verify any format
-const validAnyFormat = (anyInput, REGEX) => REGEX.test(anyInput.value);
+export default function validAnyFormat(anyInput, REGEX) {
+  return REGEX.test(anyInput.value);
+}
+
+/* Formly functions to validate forms data */
 
 // Verify the email format
 export const validEmailFormat = (emailInput) =>
   validAnyFormat(emailInput, /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/);
+
+// Verify the IPv4 format
+export const validIpv4Format = (IpInput) =>
+  validAnyFormat(
+    IpInput,
+    /^(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)$/
+  );
+
+// Verify the maximum age that the form accepts
+export const validMaxAge = (anyInput, max) => {
+  let num = Number(anyInput.value);
+
+  if (isNaN(num)) {
+    return false;
+  } else {
+    return num > max ? false : Number.isInteger(num);
+  }
+};
+
+// Verify the minimum age that the form accepts
+export const validMinAge = (anyInput, min) => {
+  let num = Number(anyInput.value);
+
+  if (isNaN(num)) {
+    return false;
+  } else {
+    return num < min ? false : Number.isInteger(num);
+  }
+};
+
+// Verify the minimum and maximum age that the form accepts
+export const validRangeAge = (anyInput, min, max) =>
+  validMaxAge(anyInput, max) && validMinAge(anyInput, min) ? true : false;
 
 // Verify the password format
 export const validPasswordFormat = (
@@ -37,54 +74,19 @@ export const validPasswordFormat = (
   return validAnyFormat(passwordInput, PASSWORD_REGEX);
 };
 
-// Verify the maximum age that the form accepts
-export const validMaxAge = (anyInput, max) => {
-  let num = Number(anyInput.value);
+// Verify the subnet mask format
+export const validMaskFormat = (MaskInput) =>
+  validAnyFormat(
+    MaskInput,
+    /^(((255\.){3}(255|254|252|248|240|224|192|128|0+))|((255\.){2}(255|254|252|248|240|224|192|128|0+)\.0)|((255\.)(255|254|252|248|240|224|192|128|0+)(\.0+){2})|((255|254|252|248|240|224|192|128|0+)(\.0+){3}))$/
+  );
 
-  if (num == NaN) {
-    return false;
-  } else {
-    if (num > max) {
-      return false;
-    } else {
-      return Number.isInteger(num);
-    }
-  }
-};
-
-// Verify the minimum age that the form accepts
-export const validMinAge = (anyInput, min) => {
-  let num = Number(anyInput.value);
-
-  if (num == NaN) {
-    return false;
-  } else {
-    if (num < min) {
-      return false;
-    } else {
-      return Number.isInteger(num);
-    }
-  }
-};
-
-// Verify if an input is empty
-export const isInputEmpty = (anyInput) =>
-  anyInput.value === "" ? true : false;
-
-// Verify the website format
+// Verify the URL format
 export const validWebsiteFormat = (websiteInput) =>
   validAnyFormat(
     websiteInput,
     /https?:\/\/(www\.)?[-a-zA-Z0-9@:%._\+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_\+.~#?&//=]*)/
   );
-
-// Verify the maximum amount of characters that the form accepts
-export const validMaxChar = (anyInput, max) =>
-  anyInput.value.length > max ? false : true;
-
-// Verify the minimum amount of characters that the form accepts
-export const validMinChar = (anyInput, min) =>
-  anyInput.value.length < min ? false : true;
 
 // Verify the username format of Twitter or GitHub
 export const validUsername = (usernameInput, sm) => {
@@ -98,12 +100,46 @@ export const validUsername = (usernameInput, sm) => {
   }
 };
 
+/* Formly functions to validate forms requirements */
+
+// Verify the exact amount of selected options that a group of checkbox accepts
+export const validExactCheckbox = (nameAttr, exact) =>
+  totalChecked(nameAttr) === exact ? true : false;
+
+// Verify the maximum amount of selected options that a group of checkbox accepts
+export const validMaxCheckbox = (nameAttr, max) =>
+  totalChecked(nameAttr) > max ? false : true;
+
+// Verify the minimum amount of selected options that a group of checkbox accepts
+export const validMinCheckbox = (nameAttr, min) =>
+  totalChecked(nameAttr) < min ? false : true;
+
+// Verify the minimum and maximum amount of selected options that a group of checkbox accepts
+export const validRangeCheckbox = (nameAttr, min, max) =>
+  totalChecked(nameAttr) < min || totalChecked(nameAttr) > max ? false : true;
+
 // Verify if an input has an exact value
 export const validExactInput = (anyInput, exactValue) =>
   anyInput.value === exactValue ? true : false;
 
+// Verify if an input is empty
+export const isInputEmpty = (anyInput) =>
+  anyInput.value === "" ? true : false;
+
+// Verify the maximum amount of characters that the form accepts
+export const validMaxChar = (anyInput, max) =>
+  anyInput.value.length > max ? false : true;
+
+// Verify the minimum amount of characters that the form accepts
+export const validMinChar = (anyInput, min) =>
+  anyInput.value.length < min ? false : true;
+
+// Verify the minimum and maximum amount of characters that the form accepts
+export const validRangeChar = (anyInput, min, max) =>
+  anyInput.value.length <= max && anyInput.value.length >= min ? true : false;
+
 // Verify if an option in a group of radio buttons is selected
-export const RadioButtonSelected = (nameAttr) => {
+export const radioButtonSelected = (nameAttr) => {
   let allInputs = document.getElementsByTagName("input");
 
   for (let i = 0; i < allInputs.length; i++) {
@@ -114,34 +150,6 @@ export const RadioButtonSelected = (nameAttr) => {
 
   return false;
 };
-
-// Verify the maximum amount of selected options that a group of checkbox accepts
-export const validMaxCheckbox = (nameAttr, max) =>
-  totalChecked(nameAttr) > max ? false : true;
-
-// Verify the minimum amount of selected options that a group of checkbox accepts
-export const validMinCheckbox = (nameAttr, min) =>
-  totalChecked(nameAttr) < min ? false : true;
-
-// Verify the exact amount of selected options that a group of checkbox accepts
-export const validExactCheckbox = (nameAttr, exact) =>
-  totalChecked(nameAttr) === exact ? true : false;
-
-// Verify the IPv4 format
-export const validIpv4Format = (IpInput) =>
-  validAnyFormat(
-    IpInput,
-    /^(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)$/
-  );
-
-// Verify the subnet mask format
-export const validMaskFormat = (MaskInput) =>
-  validAnyFormat(
-    MaskInput,
-    /^(((255\.){3}(255|254|252|248|240|224|192|128|0+))|((255\.){2}(255|254|252|248|240|224|192|128|0+)\.0)|((255\.)(255|254|252|248|240|224|192|128|0+)(\.0+){2})|((255|254|252|248|240|224|192|128|0+)(\.0+){3}))$/
-  );
-
-export default validAnyFormat;
 
 /* Auxiliary function */
 
